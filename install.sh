2f166e16fd2e27d74e051cc58eec03727611ae63
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-21 17:11:54 -0500

    maybe

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt-get install gpsd gpsd-clients python-gps -y # GPS
 sudo apt-get install python-matplotlib -y
 
 ## OpenCV
+sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
 wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.9/opencv-2.4.9.zip
 unzip opencv-2.4.9.zip
 cd opencv-2.4.9
